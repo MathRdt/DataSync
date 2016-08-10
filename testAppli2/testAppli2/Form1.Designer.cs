1ccc48e4ce46056703b15b13455d3ec2a9b4a68f
Nihal38 <anais.milhiet@gmail.com>
2016-08-10 09:32:45 +0200

    application intéractive

---
+++ testAppli2/testAppli2/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace testAppli2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelBranche = new System.Windows.Forms.Label();
+            this.labelSociete = new System.Windows.Forms.Label();
+            this.labelFamille = new System.Windows.Forms.Label();
+            this.labelSousFamille = new System.Windows.Forms.Label();
+            this.labelNomClient = new System.Windows.Forms.Label();
+            this.labelPrenomClient = new System.Windows.Forms.Label();
+            this.labelValueBranche = new System.Windows.Forms.Label();
+            this.labelValueSociete = new System.Windows.Forms.Label();
+            this.textBoxNomClient = new System.Windows.Forms.TextBox();
+            this.textBoxPrenomClient = new System.Windows.Forms.TextBox();
+            this.comboBoxFamille = new System.Windows.Forms.ComboBox();
+            this.comboBoxSousFamille = new System.Windows.Forms.ComboBox();
+            this.buttonAnnuler = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.labelApplication = new System.Windows.Forms.Label();
+            this.labelValueApplication = new System.Windows.Forms.Label();
+            this.buttonokTest = new System.Windows.Forms.Button();
+            this.labelTitre = new System.Windows.Forms.Label();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.labelAuteur = new System.Windows.Forms.Label();
+            this.textBoxTitre = new System.Windows.Forms.TextBox();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.labelValueAuteur = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Liste des méta-données :";
+            // 
+            // labelBranche
+            // 
+            this.labelBranche.AllowDrop = true;
+            this.labelBranche.AutoSize = true;
+            this.labelBranche.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBranche.Location = new System.Drawing.Point(21, 59);
+            this.labelBranche.Name = "labelBranche";
+            this.labelBranche.Size = new System.Drawing.Size(53, 13);
+            this.labelBranche.TabIndex = 1;
+            this.labelBranche.Text = "Branche :";
+            this.labelBranche.Click += new System.EventHandler(this.labelBranche_Click);
+            // 
+            // labelSociete
+            // 
+            this.labelSociete.AutoSize = true;
+            this.labelSociete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSociete.Location = new System.Drawing.Point(21, 91);
+            this.labelSociete.Name = "labelSociete";
+            this.labelSociete.Size = new System.Drawing.Size(49, 13);
+            this.labelSociete.TabIndex = 2;
+            this.labelSociete.Text = "Société :";
+            this.labelSociete.Click += new System.EventHandler(this.labelSociete_Click);
+            // 
+            // labelFamille
+            // 
+            this.labelFamille.AutoSize = true;
+            this.labelFamille.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFamille.Location = new System.Drawing.Point(21, 155);
+            this.labelFamille.Name = "labelFamille";
+            this.labelFamille.Size = new System.Drawing.Size(45, 13);
+            this.labelFamille.TabIndex = 3;
+            this.labelFamille.Text = "Famille :";
+            this.labelFamille.Click += new System.EventHandler(this.labelFamille_Click);
+            // 
+            // labelSousFamille
+            // 
+            this.labelSousFamille.AutoSize = true;
+            this.labelSousFamille.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSousFamille.Location = new System.Drawing.Point(21, 187);
+            this.labelSousFamille.Name = "labelSousFamille";
+            this.labelSousFamille.Size = new System.Drawing.Size(69, 13);
+            this.labelSousFamille.TabIndex = 4;
+            this.labelSousFamille.Text = "Sous-famille :";
+            this.labelSousFamille.Click += new System.EventHandler(this.labelSousFamille_Click);
+            // 
+            // labelNomClient
+            // 
+            this.labelNomClient.AutoSize = true;
+            this.labelNomClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNomClient.Location = new System.Drawing.Point(21, 315);
+            this.labelNomClient.Name = "labelNomClient";
+            this.labelNomClient.Size = new System.Drawing.Size(63, 13);
+            this.labelNomClient.TabIndex = 5;
+            this.labelNomClient.Text = "Nom client :";
+            this.labelNomClient.Click += new System.EventHandler(this.labelNomClient_Click);
+            // 
+            // labelPrenomClient
+            // 
+            this.labelPrenomClient.AutoSize = true;
+            this.labelPrenomClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPrenomClient.Location = new System.Drawing.Point(21, 347);
+            this.labelPrenomClient.Name = "labelPrenomClient";
+            this.labelPrenomClient.Size = new System.Drawing.Size(77, 13);
+            this.labelPrenomClient.TabIndex = 6;
+            this.labelPrenomClient.Text = "Prénom client :";
+            this.labelPrenomClient.Click += new System.EventHandler(this.labelPrenomClient_Click);
+            // 
+            // labelValueBranche
+            // 
+            this.labelValueBranche.AutoSize = true;
+            this.labelValueBranche.Location = new System.Drawing.Point(114, 59);
+            this.labelValueBranche.Name = "labelValueBranche";
+            this.labelValueBranche.Size = new System.Drawing.Size(77, 13);
+            this.labelValueBranche.TabIndex = 7;
+            this.labelValueBranche.Text = "Value Branche";
+            // 
+            // labelValueSociete
+            // 
+            this.labelValueSociete.AutoSize = true;
+            this.labelValueSociete.Location = new System.Drawing.Point(114, 91);
+            this.labelValueSociete.Name = "labelValueSociete";
+            this.labelValueSociete.Size = new System.Drawing.Size(73, 13);
+            this.labelValueSociete.TabIndex = 8;
+            this.labelValueSociete.Text = "Value Société";
+            // 
+            // textBoxNomClient
+            // 
+            this.textBoxNomClient.Location = new System.Drawing.Point(114, 308);
+            this.textBoxNomClient.Name = "textBoxNomClient";
+            this.textBoxNomClient.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNomClient.TabIndex = 9;
+            this.textBoxNomClient.TextChanged += new System.EventHandler(this.textBoxNomClient_TextChanged);
+            // 
+            // textBoxPrenomClient
+            // 
+            this.textBoxPrenomClient.Location = new System.Drawing.Point(114, 340);
+            this.textBoxPrenomClient.Name = "textBoxPrenomClient";
+            this.textBoxPrenomClient.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrenomClient.TabIndex = 10;
+            // 
+            // comboBoxFamille
+            // 
+            this.comboBoxFamille.FormattingEnabled = true;
+            this.comboBoxFamille.Location = new System.Drawing.Point(114, 147);
+            this.comboBoxFamille.Name = "comboBoxFamille";
+            this.comboBoxFamille.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxFamille.TabIndex = 11;
+            // 
+            // comboBoxSousFamille
+            // 
+            this.comboBoxSousFamille.FormattingEnabled = true;
+            this.comboBoxSousFamille.Location = new System.Drawing.Point(114, 179);
+            this.comboBoxSousFamille.Name = "comboBoxSousFamille";
+            this.comboBoxSousFamille.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSousFamille.TabIndex = 12;
+            this.comboBoxSousFamille.SelectedIndexChanged += new System.EventHandler(this.comboBoxSousFamille_SelectedIndexChanged);
+            // 
+            // buttonAnnuler
+            // 
+            this.buttonAnnuler.Location = new System.Drawing.Point(108, 392);
+            this.buttonAnnuler.Name = "buttonAnnuler";
+            this.buttonAnnuler.Size = new System.Drawing.Size(75, 23);
+            this.buttonAnnuler.TabIndex = 13;
+            this.buttonAnnuler.Text = "Annuler";
+            this.buttonAnnuler.UseVisualStyleBackColor = true;
+            this.buttonAnnuler.Click += new System.EventHandler(this.buttonAnnuler_Click);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(358, 392);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 14;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // labelApplication
+            // 
+            this.labelApplication.AutoSize = true;
+            this.labelApplication.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelApplication.Location = new System.Drawing.Point(21, 123);
+            this.labelApplication.Name = "labelApplication";
+            this.labelApplication.Size = new System.Drawing.Size(65, 13);
+            this.labelApplication.TabIndex = 15;
+            this.labelApplication.Text = "Application :";
+            // 
+            // labelValueApplication
+            // 
+            this.labelValueApplication.AutoSize = true;
+            this.labelValueApplication.Location = new System.Drawing.Point(114, 123);
+            this.labelValueApplication.Name = "labelValueApplication";
+            this.labelValueApplication.Size = new System.Drawing.Size(89, 13);
+            this.labelValueApplication.TabIndex = 16;
+            this.labelValueApplication.Text = "Value Application";
+            // 
+            // buttonokTest
+            // 
+            this.buttonokTest.Location = new System.Drawing.Point(271, 139);
+            this.buttonokTest.Name = "buttonokTest";
+            this.buttonokTest.Size = new System.Drawing.Size(36, 29);
+            this.buttonokTest.TabIndex = 17;
+            this.buttonokTest.Text = "ok";
+            this.buttonokTest.UseVisualStyleBackColor = true;
+            this.buttonokTest.Click += new System.EventHandler(this.buttonokTest_Click);
+            // 
+            // labelTitre
+            // 
+            this.labelTitre.AutoSize = true;
+            this.labelTitre.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitre.Location = new System.Drawing.Point(21, 219);
+            this.labelTitre.Name = "labelTitre";
+            this.labelTitre.Size = new System.Drawing.Size(34, 13);
+            this.labelTitre.TabIndex = 18;
+            this.labelTitre.Text = "Titre :";
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDescription.Location = new System.Drawing.Point(21, 251);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(66, 13);
+            this.labelDescription.TabIndex = 19;
+            this.labelDescription.Text = "Description :";
+            // 
+            // labelAuteur
+            // 
+            this.labelAuteur.AutoSize = true;
+            this.labelAuteur.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAuteur.Location = new System.Drawing.Point(21, 283);
+            this.labelAuteur.Name = "labelAuteur";
+            this.labelAuteur.Size = new System.Drawing.Size(44, 13);
+            this.labelAuteur.TabIndex = 20;
+            this.labelAuteur.Text = "Auteur :";
+            // 
+            // textBoxTitre
+            // 
+            this.textBoxTitre.Location = new System.Drawing.Point(114, 212);
+            this.textBoxTitre.Name = "textBoxTitre";
+            this.textBoxTitre.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTitre.TabIndex = 21;
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(114, 244);
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDescription.TabIndex = 22;
+            // 
+            // labelValueAuteur
+            // 
+            this.labelValueAuteur.AutoSize = true;
+            this.labelValueAuteur.Location = new System.Drawing.Point(114, 283);
+            this.labelValueAuteur.Name = "labelValueAuteur";
+            this.labelValueAuteur.Size = new System.Drawing.Size(132, 13);
+            this.labelValueAuteur.TabIndex = 23;
+            this.labelValueAuteur.Text = "Value Auteur = Application";
+            this.labelValueAuteur.Click += new System.EventHandler(this.labelValueAuteur_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 424);
+            this.Controls.Add(this.labelValueAuteur);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.textBoxTitre);
+            this.Controls.Add(this.labelAuteur);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.labelTitre);
+            this.Controls.Add(this.buttonokTest);
+            this.Controls.Add(this.labelValueApplication);
+            this.Controls.Add(this.labelApplication);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.buttonAnnuler);
+            this.Controls.Add(this.comboBoxSousFamille);
+            this.Controls.Add(this.comboBoxFamille);
+            this.Controls.Add(this.textBoxPrenomClient);
+            this.Controls.Add(this.textBoxNomClient);
+            this.Controls.Add(this.labelValueSociete);
+            this.Controls.Add(this.labelValueBranche);
+            this.Controls.Add(this.labelPrenomClient);
+            this.Controls.Add(this.labelNomClient);
+            this.Controls.Add(this.labelSousFamille);
+            this.Controls.Add(this.labelFamille);
+            this.Controls.Add(this.labelSociete);
+            this.Controls.Add(this.labelBranche);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Méta-données";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelBranche;
+        private System.Windows.Forms.Label labelSociete;
+        private System.Windows.Forms.Label labelFamille;
+        private System.Windows.Forms.Label labelSousFamille;
+        private System.Windows.Forms.Label labelNomClient;
+        private System.Windows.Forms.Label labelPrenomClient;
+        private System.Windows.Forms.Label labelValueBranche;
+        private System.Windows.Forms.Label labelValueSociete;
+        private System.Windows.Forms.TextBox textBoxNomClient;
+        private System.Windows.Forms.TextBox textBoxPrenomClient;
+        private System.Windows.Forms.ComboBox comboBoxFamille;
+        private System.Windows.Forms.ComboBox comboBoxSousFamille;
+        private System.Windows.Forms.Button buttonAnnuler;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Label labelApplication;
+        private System.Windows.Forms.Label labelValueApplication;
+        private System.Windows.Forms.Button buttonokTest;
+        private System.Windows.Forms.Label labelTitre;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.Label labelAuteur;
+        private System.Windows.Forms.TextBox textBoxTitre;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.Label labelValueAuteur;
+    }
+}
+
---
+++ testAppli2/testAppli2/Form1.cs
@@ -0,0 +1,209 @@
+﻿//fonction module manuel afin de récupérer les méta-données manquantes
+
+using System;
+using System.Windows.Forms;
+
+
+namespace testAppli2
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            // a rentrer par rapport à la valeur du xml
+
+            //liste méta-données que l'on a déjà
+            string[] metaData = { "Value Branche", "Value Société", "Value Application", " ", "" };
+            //liste des différentes familles existantes
+            string[] FamilleList = { "Facture", "Paye" };
+            //liste des différentes sous-famille existante en fonction de la famille
+            string[] SousFamilleFacture = { "Facture client", "Facture interne" };
+            string[] SousFamillePaye = { "Paye type 1", "Paye type 2" };
+            //méta-données facultatives
+            string[] MetaFalcSousFamilleFac = { "Nom", "Prénom", "facultative1" };
+            string[] MetaFalcSousFamillePaye = { "Nom", "Prénom", "facultative2" };
+
+            //branche, société, application ne peuvent pas être modifié par l'utilisateur
+            //affichage de ces méta-données
+            labelValueBranche.Text = metaData[0];
+            labelValueSociete.Text = metaData[1];
+            labelValueApplication.Text = metaData[2];
+
+            //si la famille n'existe pas, l'utilisateur choisit une famille parmis la liste des familles existantes
+            //création d'une combobox qui permet d'effectuer un choix parmis une liste
+            if (metaData[3] != " " && metaData[3] != "")
+            {
+                //si la famille a été trouvé suppression de la liste déroulante
+                comboBoxFamille.Dispose();
+                //création d'un label pour afficher le résultat
+                Label textbox45 = new Label();
+                textbox45.Left = 115;
+                textbox45.Top = 150;
+                textbox45.Width = 120;
+                textbox45.Height = 30;
+                this.Controls.Add(textbox45);
+                //ce label correspond à la méta-données famille qui est la 4eme de la liste
+                textbox45.Text = metaData[3];
+            }
+            else
+            {
+                //si la famille a été trouvé proposition des famille sous forme de menu déroulant
+                comboBoxFamille.Items.Clear();
+                for (int i = 0; i < FamilleList.Length; i++)
+                {
+                    comboBoxFamille.Items.Add(FamilleList[i]);
+                }
+            }
+
+
+            //si la sous famille n'existe pas l'utilisateur choisi une sous-famille parmi la liste des sous-familles existantes 
+            if(metaData[4] != " " && metaData[4] != "")
+            {
+                //si la sous-famille est connue suppression de la liste déroulante
+                comboBoxSousFamille.Dispose();
+                //création d'un label pour afficher le résultat
+                Label textbox45 = new Label();
+                textbox45.Left = 115;
+                textbox45.Top = 205;
+                textbox45.Width = 120;
+                textbox45.Height = 30;
+                this.Controls.Add(textbox45);
+                //si l'on connait la sous-famille elle est la 5eme string de la liste
+                textbox45.Text = metaData[4];
+                
+            }
+
+            //meta données facultatives
+            textBoxNomClient.Text = "Dupond";
+            textBoxPrenomClient.Text = "Pierre";
+
+            //récupération des méta-données trouvé
+            //ordre remplissage : branche, société, application, famille, sous-famille, titre, description, auteur, nom, prénom 
+            //a tester
+            string[] metaDatas = new string[30];
+            metaDatas[0] = labelValueBranche.Text;//branche
+            metaDatas[1] = labelValueSociete.Text;//société
+            metaDatas[2] = labelValueApplication.Text;//application
+            if (metaData[3] != " " && metaData[3] != "") metaDatas[3] = metaData[3];//famille
+            else metaDatas[3] = comboBoxFamille.Text;
+
+            if (metaData[4] != " " && metaData[4] != "") metaDatas[4] = metaData[4];//sous-famille
+            else metaDatas[4] = comboBoxSousFamille.Text;
+
+            metaDatas[5] = textBoxTitre.Text;//titre
+            metaDatas[6] = textBoxDescription.Text;//description
+            metaDatas[7] = labelApplication.Text;//auteur = application
+            metaDatas[8] = textBoxNomClient.Text;//nom client
+            metaDatas[9] = textBoxPrenomClient.Text;//prénom
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void labelSociete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelFamille_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelBranche_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelSousFamille_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelNomClient_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelPrenomClient_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //si l'utilisateur clique sur le bouton annuler la fenêtre est fermée
+        private void buttonAnnuler_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        //si l'utilisateur clique sur le bouton ok affichage d'une fenêtre demandant si l'utilisateur est sûr de vouloir quitter
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            //si oui la fenêtre se ferme
+            //sinon on revient sur la page
+            if (MessageBox.Show("Etes-vous sûr de vouloir quitter ?", "Quitter",
+         MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void textBoxNomClient_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        //lorsque l'utilisateur clique sur la croix rouge lui demande s'il est sûr de vouloir quitter
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Etes-vous certain de vouloir quitter ?", "Quitter", MessageBoxButtons.YesNo) == DialogResult.No)
+                e.Cancel = true;
+        }
+
+        //affiche la sous famille correspondant à la famille sélectionnée lorsque l'utilisateur clique sur le bouton ok à coté de famille
+        private void buttonokTest_Click(object sender, EventArgs e)
+        {
+            string[] SousFamilleFacture = { "Facture client", "Facture interne" };
+            string[] SousFamillePaye = { "Paye type 1", "Paye type 2" };
+
+            //en fonction de la famille choisi va s'afficher le menu correspondant des sous familles
+            if (comboBoxFamille.Text == "Facture")
+            {
+                comboBoxSousFamille.Items.Clear();
+                for (int i = 0; i < SousFamilleFacture.Length; i++)
+                {
+                    comboBoxSousFamille.Items.Add(SousFamilleFacture[i]);
+                }
+            }
+            else if (comboBoxFamille.Text == "Paye")
+            {
+                comboBoxSousFamille.Items.Clear();
+                for (int i = 0; i < SousFamillePaye.Length; i++)
+                {
+                    comboBoxSousFamille.Items.Add(SousFamillePaye[i]);
+                }
+            }
+        }
+
+        private void comboBoxSousFamille_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void okSousFamille_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelValueAuteur_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
